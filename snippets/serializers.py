f3d5e3ed2b9636fe4128449b7889531629d5222f
Brix John Novesteras <brixjohnovesteras@gmail.com>
2023-06-27 21:53:09 +0800

    Updating Our Serializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,6 +4,8 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")  # new
+    
     class Meta:
         model = Snippet
         fields = (
@@ -13,6 +15,7 @@ class SnippetSerializer(serializers.ModelSerializer):
             "linenos",
             "language",
             "style",
+            "owner",
         )
 
 
