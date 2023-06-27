91608cab8bfcc510be35ab350c830d6796ba2b06
Brix John Novesteras <brixjohnovesteras@gmail.com>
2023-06-27 21:49:07 +0800

    Associating Snippets with Users

--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,8 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):  # new
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
